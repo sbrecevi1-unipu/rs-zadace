faf809f20485643167462e30386e55ceb3a10c30
Samuel <samujele@gmail.com>
2024-11-14 07:49:18 +0100

    Vježba 7 dovršena

--- vjezba7.py
+++ vjezba7.py
@@ -3,13 +3,17 @@ def verifikacija(poruka, lozinka):
         return 'Lozinka mora sadržavati između 8 i 15 znakova '
     elif not any(i.isupper() for i in lozinka) or not any(i.isdigit() for i in lozinka):
         return 'Lozinka mora sadržavati barem jedno veliko slovo i jedan broj\n'
-    return 'Lozinka je ispravna'
+    elif lozinka.lower().find('lozinka')!=-1 or lozinka.lower().find('password')!=-1:
+        return "Lozinka ne smije sadržavati riječi 'password' ili 'lozinka' \n"
+    else:
+        return "Lozinka je jaka!"
 
 neispravna = True
 poruka = 'Upiši lozinku '
 while neispravna:
     lozinka = input(poruka)
     poruka = verifikacija(poruka, lozinka)
-    print(poruka)
-    if poruka == 'Lozinka je ispravna':
+    # print(poruka)
+    if poruka == 'Lozinka je jaka!':
+        print(poruka)
         neispravna = False
