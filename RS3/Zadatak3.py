e2b98429fc0b78061067532d7cf1449b3ad27f38
Samuel Brecevic <samujele@gmail.com>
2024-12-01 18:36:12 +0100

    Zadatak4 finito.

--- RS3/Zadatak3.py
+++ RS3/Zadatak3.py
@@ -8,6 +8,15 @@ poslužiteljskoj strani. Korutina kao ulazni parametar prima rječnik koji opisu
 od ključeva korisnicko_ime , email i lozinka . Unutar korutine simulirajte provjeru korisničkog
 imena na način da ćete provjeriti nalaze li se par korisnicko_ime i email u bazi korisnika. Ova
 provjera traje 3 sekunde.
+Ako se korisnik ne nalazi u bazi, vratite poruku "Korisnik {korisnik} nije pronađen."
+Ako se korisnik nalazi u bazi, potrebno je pozvati vanjsku korutinu autorizacija() koja će simulirati
+autorizaciju korisnika u trajanju od 2 sekunde. Funkcija kao ulazni parametar prima rječnik korisnika iz baze
+i lozinku proslijeđenu iz korutine autentifikacija() . Autorizacija simulira dekripciju lozinke (samo
+provjerite podudaranje stringova) i provjeru s lozinkom iz baze_lozinka. Ako su lozinke jednake, korutine
+vraća poruku "Korisnik {korisnik}: Autorizacija uspješna." , a u suprotnom "Korisnik
+{korisnik}: Autorizacija neuspješna." .
+
+Korutinu autentifikacija() pozovite u main() funkciji s proizvoljnim korisnikom i lozinkom.
 '''
 
 baza_korisnika = [
---
+++ RS3/Zadatak4.py
@@ -0,0 +1,52 @@
+import asyncio
+import time
+import json
+import random
+
+'''
+4. Definirajte korutinu provjeri_parnost koja će simulirati "super zahtjevnu operaciju" provjere
+parnosti broja putem vanjskog API-ja. Korutina prima kao argument broj za koji treba provjeriti
+parnost, a vraća poruku "Broj {broj} je paran." ili "Broj {broj} je neparan." nakon 2 sekunde.
+Unutar main funkcije definirajte listu 10 nasumičnih brojeva u rasponu od 1 do 100 (koristite random
+modul). Listu brojeva izgradite kroz list comprehension sintaksu. Nakon toga, pohranite u listu zadaci
+10 Task objekata koji će izvršavati korutinu provjeri_parnost za svaki broj iz liste (također kroz list
+comprehension). Na kraju, koristeći asyncio.gather() , pokrenite sve korutine konkurentno i ispišite
+rezultate.
+'''
+
+
+
+
+async def provjeri_parnost(broj):
+    await asyncio.sleep(2)  # Simulate decryption process
+    
+    if broj % 2 == 0:
+        result =  f"Broj {broj} je paran."
+    else:
+        result =  f"Broj {broj} je neparan."
+            
+    return result
+
+
+    await asyncio.sleep(3)
+    for korisnik_iz_baze in baza_korisnika:
+        if korisnik_iz_baze["korisnicko_ime"] == korisnik["korisnicko_ime"] and korisnik_iz_baze["email"] == korisnik["email"]:
+            print(await autorizacija(korisnik_iz_baze, korisnik['lozinka']))
+            return f"Korisnik {korisnik['korisnicko_ime']} je pronađen."  
+        else:
+            return f"Korisnik {korisnik['korisnicko_ime']} nije pronađen."  
+    
+ 
+async def main():
+    
+    start_time = time.time()
+    
+    lista_brojeva = [random.randint(1, 100)  for x in range(10)]
+    zadaci = [asyncio.create_task(provjeri_parnost(x)) for x in lista_brojeva]
+    print(await asyncio.gather(* zadaci))
+    
+    end_time = time.time()
+    time_difference = end_time - start_time
+    print(f"Završeno u {time_difference : .0f} sekunde/i")
+
+asyncio.run(main())
